031c5292649975d24b8f203f90fddb3e30ca1e7d
ywalakamar <barxwells@gmail.com>
2019-12-08 14:37:56 +0300

    Using dummy data to simulate the API data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
 
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
 class App extends Component {
   render() {
-    return <h1>Welcome to Hacker News</h1>;
+    return (
+      <div className="App">
+        <table>
+          <thead>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Comments</th>
+            <th>Points</th>
+          </thead>
+          <tbody>
+            {list.map(item => {
+              return (
+                <tr key={item.objectID}>
+                  <td>{item.objectID}</td>
+                  <td>{item.title}</td>
+                  <td>{item.author}</td>
+                  <td>{item.num_comments}</td>
+                  <td>{item.points}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
