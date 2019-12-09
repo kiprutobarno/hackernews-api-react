b7ad1e9c619885652480f1fadd339595c537499b
ywalakamar <barxwells@gmail.com>
2019-12-09 12:12:09 +0300

    Test Table component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import App, { Search, Button } from "./App";
+import App, { Search, Button, Table } from "./App";
 
 describe("App", () => {
   it("renders without crashing", () => {
@@ -39,6 +39,31 @@ describe("Button", () => {
   });
   test("has a valid snapshot", () => {
     const component = renderer.create(<Button>Give me more</Button>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Table", () => {
+  const props = {
+    list: [
+      { title: "Metaphysics", author: "Barno", num_comments: 6, objectID: "B" },
+      {
+        title: "Biosystems",
+        author: "Kimberly",
+        num_comments: 2,
+        objectID: "X"
+      }
+    ]
+  };
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Table {...props} />, div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Table {...props} />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
